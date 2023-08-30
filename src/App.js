2a249a9236bbd7021fd240a822ba4cf9b77a4e07
Valentino <valentinoindorato@gmail.com>
2023-08-30 09:57:39 -0300

    PreEntrega1+Indorat

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import NavBar from '../src/Components/NavBar';// No logro identificar porque me marca error
+import ItemListContainer from '../src/Components/ItemListContainer';
+import './App.css';
+
+function App() {
+  return (
+    <  >
+      <NavBar />
+      <ItemListContainer greeting="Bienvenido al Mercado Norte" />
+    </>
+  );
+}
+
+export default App;
